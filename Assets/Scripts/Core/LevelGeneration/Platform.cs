f644eb1e4be2e39bed6a670c065c8a9cb2d7ea71
Andrii <101969951+MechanikXD@users.noreply.github.com>
2025-07-28 13:45:25 +0300

    Add player i-frames. Level movement/generation.

---
+++ Assets/Scripts/Core/LevelGeneration/Platform.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Core.LevelGeneration {
+    public class Platform : MonoBehaviour {
+        [SerializeField] private float _platformLength;
+        // [SerializeField] private Transform[] _obstacleSpawnPoints;
+        // [SerializeField] private Obstacle[] _obstaclePool;
+        public float Length => _platformLength;
+        public Vector3 Position => transform.position;
+
+        public Platform InstantiateNew(Vector3 pos, Transform parent) {
+            return Instantiate(gameObject, pos, Quaternion.identity, parent).GetComponent<Platform>();
+        }
+        public void ClearObject() {}
+        public void PlaceRandomObject() {}
+
+        public void MoveTo(Vector3 position) {
+            transform.localPosition = position;
+        }
+    }
+}
