13aaab70d781b22a4bf947645884f28314f07c91
Andrii <101969951+MechanikXD@users.noreply.github.com>
2025-07-28 11:59:13 +0300

    Better PlayerController.cs based on target positions. Add scene changes.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,5 @@
-using DG.Tweening;
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,84 +9,152 @@ namespace Player {
         [SerializeField] private Transform _leftPosition;
         [SerializeField] private Transform _middlePosition;
         [SerializeField] private Transform _rightPosition;
-        private Tween _currentMovement;
         private int _lastPlatformPositions;
+        
+        // Target values
+        private Vector3 _targetPosition;
+        private Vector3 _targetScale = Vector3.one;
+        // Movement state
+        private bool _isMoving;
+        private bool _isScaling;
+        private bool _isJumpingOrCrouching;
 
-        [SerializeField] private float _repositionTime;
+        private float _originalHeight;
+
+        private const float CalcError = 0.0001f;
+
+        [SerializeField] private float _moveSpeed;
         [SerializeField] private float _jumpDuration;
-        [SerializeField] private Vector3 _jumpElevation;
-        [SerializeField] private float _crouchTime;
+        [SerializeField] private float _jumpElevation;
+        private bool _canJump = true;
+        [SerializeField] private float _crouchDuration;
+        [SerializeField] private float _scaleSpeed;
         [SerializeField] private Vector3 _crouchScale;
+        private bool _canCrouch = true;
 
         private void Awake() {
             // Start at middle platform.
-            MoveToMiddlePlatform(0f);
+            var middle = _middlePosition.localPosition;
+            SetDesiredHeight(middle.y, true);
+            SetDesiredPosition(middle, true);
+            _originalHeight = transform.position.y;
         }
 
-        private void MoveToLeftPlatform(float duration) {
-            // TODO: Buffer or take over last movement
-            if (_currentMovement.IsActive()) return;
+        private void Update() {
+            // Movement
+            var distanceToTarget = Vector3.Distance(transform.position, _targetPosition);
+
+            if (distanceToTarget > CalcError) {
+                _isMoving = true;
+                transform.position =
+                    Vector3.Lerp(transform.position, _targetPosition, _moveSpeed * Time.deltaTime);
+            }
+            else if (_isMoving) {
+                // Snap when close enough
+                _isMoving = false;
+                transform.position = _targetPosition;
+            }
+
+            // Scale
+            var scaleDifference = Vector3.Distance(transform.position, _targetPosition);
+
+            if (scaleDifference > CalcError) {
+                _isScaling = true;
+                transform.localScale =
+                    Vector3.Lerp(transform.localScale, _targetScale, _scaleSpeed * Time.deltaTime);
+            }
+            else if (_isScaling) {
+                // Snap when close enough
+                _isScaling = false;
+                transform.localScale = _targetScale;
+            }
+        }
+
+        private void SetDesiredPosition(Vector3 position, bool isInstant = false) {
+            position = new Vector3(position.x, _targetPosition.y, position.z);
+
+            if (isInstant) {
+                transform.position = position;
+                _targetPosition = position;
+            }
+            else _targetPosition = position;
+        }
+
+        private void SetDesiredHeight(float y, bool isInstant = false) {
+            if (isInstant) {
+                var currentTransform = transform;
+                var position = currentTransform.position;
+                position.y = y;
+                currentTransform.position = position;
+                _targetPosition.y = y;
+            }
+            else {
+                _targetPosition.y = y;
+            }
+        }
+
+        private void SetDesiredScale(Vector3 scale, bool isInstant = false) {
+            if (isInstant) {
+                transform.localScale = scale;
+                _targetScale = scale;
+            }
+            else _targetScale = scale;
+        }
+
+        private void Jump() {
+            if (_isJumpingOrCrouching || !_canJump) return;
             
-            _currentMovement = transform.DOMove(_leftPosition.localPosition, duration);
-            _lastPlatformPositions = -1;
-        }
-
-        private void MoveToMiddlePlatform(float duration) {
-            if (_currentMovement.IsActive()) return;
+            SetDesiredHeight(_originalHeight + _jumpElevation);
+            _isJumpingOrCrouching = true;
+            _canJump = false;
             
-            _currentMovement = transform.DOMove(_middlePosition.localPosition, duration);
-            _lastPlatformPositions = 0;
+            IEnumerator SetOriginalHeightLater() {
+                yield return new WaitForSeconds(_jumpDuration);
+                SetDesiredHeight(_originalHeight);
+                _isJumpingOrCrouching = false;
+                yield return new WaitUntil(AtOriginalHeight);
+                _canJump = true;
+            }
+
+            StartCoroutine(SetOriginalHeightLater());
         }
 
-        private void MoveToRightPlatform(float duration) {
-            if (_currentMovement.IsActive()) return;
-            
-            _currentMovement = transform.DOMove(_rightPosition.localPosition, duration);
-            _lastPlatformPositions = 1;
-        }
-
-        private void TryJump() {
-            if (_currentMovement.IsActive()) return;
-
-            var originalPosition = transform.position;
-            var newPosition = originalPosition + _jumpElevation;
-            
-            var animationSequence = DOTween.Sequence();
-            animationSequence.Append(transform.DOMove(newPosition, _repositionTime));
-            animationSequence.Append(transform.DOMove(originalPosition, _repositionTime).SetDelay(_jumpDuration));
-
-            _currentMovement = animationSequence.Play();
-        }
-        
         private void TryCrouch() {
-            if (_currentMovement.IsActive()) return;
-
-            var playerTransform = transform;
-            var originalScale = playerTransform.localScale;
-            var originalPosition = playerTransform.position;
-            var adjustedPosition = originalPosition;
-            adjustedPosition.y /= 2f;
+            if (_isJumpingOrCrouching || !_canCrouch) return;
             
-            var animationSequence = DOTween.Sequence();
-            animationSequence.Append(transform.DOScale(_crouchScale, _repositionTime)
-                .OnComplete(() => transform.position = adjustedPosition));
-            animationSequence.Append(transform.DOScale(originalScale, _repositionTime)
-                .SetDelay(_crouchTime).OnComplete(() => transform.position = originalPosition));
+            SetDesiredHeight(_crouchScale.y / 2);
+            SetDesiredScale(_crouchScale);
+            _isJumpingOrCrouching = true;
+            _canCrouch = false;
+            
+            IEnumerator SetOriginalHeightLater() {
+                yield return new WaitForSeconds(_crouchDuration);
+                SetDesiredHeight(_originalHeight);
+                SetDesiredScale(Vector3.one);   // None: scale must be vector.one by default (like it should be)
+                _isJumpingOrCrouching = false;
+                yield return new WaitUntil(AtOriginalHeight);
+                _canCrouch = true;
+            }
 
-            _currentMovement = animationSequence.Play();
+            StartCoroutine(SetOriginalHeightLater());
         }
+
+        private bool AtOriginalHeight() => Math.Abs(transform.position.y - _originalHeight) < 0.01f;
 
         #region Input System Events
 
         public void OnLeft() {
             switch (_lastPlatformPositions) {
                 case 1:
-                    MoveToMiddlePlatform(_repositionTime);
+                    SetDesiredPosition(_middlePosition.localPosition);
+                    _lastPlatformPositions = 0;
                     break;
                 case 0:
-                    MoveToLeftPlatform(_repositionTime);
+                    SetDesiredPosition(_leftPosition.localPosition);
+                    _lastPlatformPositions = -1;
                     break;
                 case -1:
+                    // Already there
                     break;
             }
         }
@@ -93,17 +162,20 @@ namespace Player {
         public void OnRight() {
             switch (_lastPlatformPositions) {
                 case 1:
+                    // Already there
                     break;
                 case 0:
-                    MoveToRightPlatform(_repositionTime);
+                    SetDesiredPosition(_rightPosition.localPosition);
+                    _lastPlatformPositions = 1;
                     break;
                 case -1:
-                    MoveToMiddlePlatform(_repositionTime);
+                    SetDesiredPosition(_middlePosition.localPosition);
+                    _lastPlatformPositions = 0;
                     break;
             }
         }
 
-        public void OnUp() => TryJump();
+        public void OnUp() => Jump();
 
         public void OnDown() => TryCrouch();
 
