dc4caa775a9a295fad25e912093ad1fcd423edfc
Andrii <101969951+MechanikXD@users.noreply.github.com>
2025-07-29 11:51:16 +0300

    Add empty settings screen.

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -8,14 +8,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace UI {
-    public class UIManager : MonoBehaviour {
+    public class UIManager : MonoBehaviour { 
         private static PauseMenuView _pauseMenu;
         private static GameOverView _gameOver;
+        private static SettingsView _settingsView;
 
         private void Awake() {
             var sceneCanvases = GameObject.FindGameObjectsWithTag("Canvas");
             _ = sceneCanvases.First(canvas => canvas.TryGetComponent(out _pauseMenu));
             _ = sceneCanvases.First(canvas => canvas.TryGetComponent(out _gameOver));
+            _ = sceneCanvases.First(canvas => canvas.TryGetComponent(out _settingsView));
             
             OnHideCanvas();
         }
@@ -23,25 +25,32 @@ namespace UI {
         private void Start() {
             _pauseMenu.HideCanvas();
             _gameOver.HideCanvas();
+            _settingsView.HideCanvas();
         }
 
         private void OnEnable() {
             _pauseMenu.ResumePressed += HidePauseMenu;
             _pauseMenu.RestartPressed += RestartScene;
             _pauseMenu.ExitPressed += ExitApp;
+            _pauseMenu.SettingsPressed += ShowSettings;
 
             _gameOver.RestartPressed += RestartScene;
             _gameOver.ExitPressed += ExitApp;
             PlayerController.Defeated += ShowGameOver;
+
+            _settingsView.BackFromSettings += ReturnFromSettings;
         }
 
         private void OnDisable() {
             _pauseMenu.ResumePressed -= HidePauseMenu;
             _pauseMenu.RestartPressed -= RestartScene;
             _pauseMenu.ExitPressed -= ExitApp;
+            _pauseMenu.SettingsPressed -= ShowSettings;
             
             _gameOver.RestartPressed -= RestartScene;
             _gameOver.ExitPressed -= ExitApp;
+            
+            _settingsView.BackFromSettings -= ReturnFromSettings;
         }
 
         private static void OnShowCanvas() {
@@ -63,17 +72,29 @@ namespace UI {
             _pauseMenu.ShowCanvas();
         }
 
-        private void HidePauseMenu() {
+        private static void HidePauseMenu() {
             if (_gameOver.IsEnabled) return;
             
             OnHideCanvas();
             _pauseMenu.HideCanvas();
         }
 
-        public void ShowGameOver() {
+        private static void ShowGameOver() {
             // TODO: Display current and best scores
             OnShowCanvas();
             _gameOver.ShowCanvas();
+        }
+
+        private static void ShowSettings() {
+            // NOTE: Since only access to settings is from pause menu (for now) it's hardcoded to return to it.
+            _pauseMenu.HideCanvas();
+            _settingsView.ShowCanvas();
+        }
+
+        private static void ReturnFromSettings() {
+            // NOTE: Since only access to settings is from pause menu (for now) it's hardcoded to return to it.
+            _settingsView.HideCanvas();
+            _pauseMenu.ShowCanvas();
         }
         
         // TODO: Move somewhere where those methods belong to
--- Assets/Scripts/UI/Views/PauseMenuView.cs
+++ Assets/Scripts/UI/Views/PauseMenuView.cs
@@ -6,12 +6,14 @@ namespace UI.Views {
     public class PauseMenuView : MonoBehaviour {
         [SerializeField] private Button _resumeButton;
         [SerializeField] private Button _restartButton;
+        [SerializeField] private Button _settingsButton;
         [SerializeField] private Button _exitButton;
         private Canvas _thisCanvas;
         private Action _unsubscribeFromEvents;
 
         public event Action ResumePressed;
         public event Action RestartPressed;
+        public event Action SettingsPressed;
         public event Action ExitPressed;
 
         private void Awake() => _thisCanvas = GetComponent<Canvas>();
@@ -20,15 +22,18 @@ namespace UI.Views {
             void OnResumePressed() => ResumePressed?.Invoke();
             void OnRestartPressed() => RestartPressed?.Invoke();
             void OnExitPressed() => ExitPressed?.Invoke();
+            void OnSettingsPressed() => SettingsPressed?.Invoke();
             
             _resumeButton.onClick.AddListener(OnResumePressed);
             _restartButton.onClick.AddListener(OnRestartPressed);
             _exitButton.onClick.AddListener(OnExitPressed);
+            _settingsButton.onClick.AddListener(OnSettingsPressed);
 
             _unsubscribeFromEvents = () => {
                 _resumeButton.onClick.RemoveListener(OnResumePressed);
                 _restartButton.onClick.RemoveListener(OnRestartPressed);
                 _exitButton.onClick.RemoveListener(OnExitPressed);
+                _settingsButton.onClick.RemoveListener(OnSettingsPressed);
             };
         }
 
