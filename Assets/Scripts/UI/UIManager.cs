eb22556045f3704acfb278015e5ed47ad5ed1952
Andrii <101969951+MechanikXD@users.noreply.github.com>
2025-07-29 11:29:49 +0300

    Make Game Over screen.

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
+using Player;
 using UI.Views;
 #if UNITY_EDITOR
 using UnityEditor;
@@ -10,60 +10,71 @@ using UnityEngine.SceneManagement;
 namespace UI {
     public class UIManager : MonoBehaviour {
         private static PauseMenuView _pauseMenu;
-        // [SerializeField] private GameOverView _gameOver;
+        private static GameOverView _gameOver;
 
         private void Awake() {
             var sceneCanvases = GameObject.FindGameObjectsWithTag("Canvas");
             _ = sceneCanvases.First(canvas => canvas.TryGetComponent(out _pauseMenu));
+            _ = sceneCanvases.First(canvas => canvas.TryGetComponent(out _gameOver));
             
             OnHideCanvas();
         }
 
         private void Start() {
             _pauseMenu.HideCanvas();
+            _gameOver.HideCanvas();
         }
 
         private void OnEnable() {
             _pauseMenu.ResumePressed += HidePauseMenu;
             _pauseMenu.RestartPressed += RestartScene;
             _pauseMenu.ExitPressed += ExitApp;
+
+            _gameOver.RestartPressed += RestartScene;
+            _gameOver.ExitPressed += ExitApp;
+            PlayerController.Defeated += ShowGameOver;
         }
 
         private void OnDisable() {
             _pauseMenu.ResumePressed -= HidePauseMenu;
             _pauseMenu.RestartPressed -= RestartScene;
             _pauseMenu.ExitPressed -= ExitApp;
+            
+            _gameOver.RestartPressed -= RestartScene;
+            _gameOver.ExitPressed -= ExitApp;
         }
 
         private static void OnShowCanvas() {
+            Time.timeScale = 0f;
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.Confined;
         }
 
         private static void OnHideCanvas() {
+            Time.timeScale = 1f;
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
         }
 
         public static void ShowPauseMenu() {
-            // if (_gameOver.IsEnabled) return;
+            if (_gameOver.IsEnabled) return;
             
             OnShowCanvas();
             _pauseMenu.ShowCanvas();
         }
 
         private void HidePauseMenu() {
-            // if (_gameOver.IsEnabled) return;
+            if (_gameOver.IsEnabled) return;
             
             OnHideCanvas();
             _pauseMenu.HideCanvas();
         }
 
-        /*public void ShowGameOver(bool isEnabled) {
+        public void ShowGameOver() {
+            // TODO: Display current and best scores
             OnShowCanvas();
-            if (isEnabled) _gameOver.ShowCanvas();
-            else _gameOver.HideCanvas();
-        }*/
+            _gameOver.ShowCanvas();
+        }
         
         // TODO: Move somewhere where those methods belong to
         private void ExitApp() {
--- Assets/Scripts/UI/Views/GameOverView.cs
+++ Assets/Scripts/UI/Views/GameOverView.cs
@@ -1,12 +1,41 @@
-﻿using UnityEngine;
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI.Views {
     public class GameOverView : MonoBehaviour {
+        [SerializeField] private Button _restartButton;
+        [SerializeField] private Button _exitButton;
+        [SerializeField] private TMP_Text _bestScore;
+        [SerializeField] private TMP_Text _currentScore;
         private Canvas _thisCanvas;
+        private Action _unsubscribeFromEvents;
         public bool IsEnabled => _thisCanvas.enabled;
+        
+        public event Action RestartPressed;
+        public event Action ExitPressed;
 
-        private void Awake() {
-            _thisCanvas = GetComponent<Canvas>();
+        private void Awake() => _thisCanvas = GetComponent<Canvas>();
+
+        private void OnEnable() {
+            void OnRestartPressed() => RestartPressed?.Invoke();
+            void OnExitPressed() => ExitPressed?.Invoke();
+            
+            _restartButton.onClick.AddListener(OnRestartPressed);
+            _exitButton.onClick.AddListener(OnExitPressed);
+
+            _unsubscribeFromEvents = () => {
+                _restartButton.onClick.RemoveListener(OnRestartPressed);
+                _exitButton.onClick.RemoveListener(OnExitPressed);
+            };
+        }
+
+        private void OnDisable() => _unsubscribeFromEvents();
+
+        public void SetScores(int currentScore, int bestScore) {
+            _currentScore.SetText(currentScore.ToString());
+            _bestScore.SetText(bestScore.ToString());
         }
         
         public void ShowCanvas() => _thisCanvas.enabled = true;
